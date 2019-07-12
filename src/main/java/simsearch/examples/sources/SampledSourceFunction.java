01b33a5c98256e8c6fdecf04b2da3aef4a42ae05
arianeziehn <arianeziehn@googlemail.com>
2019-07-12 13:49:19 +0200

    Renaming classes, add readme

---
+++ src/main/java/simsearch/examples/sources/SampledSourceFunction.java
@@ -0,0 +1,106 @@
+package simsearch.examples.sources;
+
+
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import simsearch.examples.util.KeyedDataPoint;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class SampledSourceFunction implements SourceFunction<KeyedDataPoint<Double>> {
+
+
+    private volatile boolean isRunning = true;
+
+    private String key;
+    private String dataFileName;
+    private long samplingRate = 0L;
+
+    private volatile long currentTimeMs = -1;
+    private Logger LOG = LoggerFactory.getLogger(SampledSourceFunction.class);
+
+    public SampledSourceFunction(String keyValue, String fileName, long samplingRate) {
+        this.key = keyValue;
+        this.dataFileName = fileName;
+        this.samplingRate = samplingRate;
+
+    }
+
+    public void run(SourceFunction.SourceContext<KeyedDataPoint<Double>> sourceContext) {
+        // Set an arbitrary starting time, or set one fixed one to always generate the same time stamps (for testing)
+        long startTime = 0L;//System.currentTimeMillis(); //calendar.getTimeInMillis();
+
+       /* Get the Stream of sampled data Elements in the File:
+        // Here it simulates streaming
+        // Here the program simulates stream source, reading the file 10 times*/
+
+        double periodMs = Math.round((1.0 / this.samplingRate) * 1000);  // period in miliseconds
+
+        double val = 0.0;
+
+        Scanner scan;
+        File file = new File(this.dataFileName);
+
+        try {
+            scan = new Scanner(file);
+            int count = 1;
+            int dpCounter = 0;
+            int counterS = 0;
+            long timeS = System.currentTimeMillis();
+
+            while (scan.hasNext()) {
+
+                try {
+                    val = Double.parseDouble(scan.next());
+                } catch (NumberFormatException nfe) {
+
+                    val = -0.023;  // replace mising with the ECG mean
+                }
+                if (this.currentTimeMs == -1) {
+                    this.currentTimeMs = startTime;
+                } else {
+                    this.currentTimeMs = (long) (this.currentTimeMs + periodMs);
+                }
+
+
+                KeyedDataPoint<Double> point = new KeyedDataPoint<Double>(this.key, this.currentTimeMs, val);
+                sourceContext.collect(point);
+                dpCounter++;
+
+                if (System.currentTimeMillis() - timeS > 1000) {
+                    counterS++;
+                    LOG.info(dpCounter + "/s" + "in day" + count + " s " + counterS + " key" + this.key + " time " + System.currentTimeMillis());
+
+                    dpCounter = 0;
+                    timeS = System.currentTimeMillis();
+                }
+
+
+                if (!scan.hasNext() && count <= 9) {
+                    LOG.info("Read File finished, start again " + count);
+                    scan = new Scanner(file);
+                    count++;
+                    counterS = 0;
+                }
+
+
+            }
+            scan.close();
+
+        } catch (FileNotFoundException e1) {
+            LOG.error("File Not FOUND :" + this.dataFileName);
+            // e1.printStackTrace();
+        }
+
+
+    }
+
+    public void cancel() {
+        this.isRunning = false;
+    }
+
+
+}
